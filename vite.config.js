f3723eb5802366a31e57c4a4a52c336c427df949
shimizu <xxshimizuxx@gmail.com>
2025-10-30 14:27:14 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
+
+export default defineConfig({
+  root: 'src',
+  base: "./",
+  publicDir: '../public',
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
+  server: {
+    open: true
+  },
+  build: {
+    outDir: '../dist'
+  }
+})
